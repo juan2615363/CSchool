66902e6aa881b0aca2897e4cca548375700f9d38
juan2615363 <834447647@qq.com>
2017-05-16 12:12:51 +0800

    测试单元

---
+++ app/src/main/java/cn/songguohulian/cschool/ui/MGitDemo.java
@@ -0,0 +1,10 @@
+package cn.songguohulian.cschool.ui;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/5/16.
+ */
+
+public class MGitDemo extends AppCompatActivity {
+}
